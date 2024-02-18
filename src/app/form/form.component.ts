65a41a27bd43ce4673f2b1e04011df03da18aa4c
ares8 <ares10801@gmail.com>
2024-02-18 10:13:41 +0100

    projectv29

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Login } from '../models';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss',
 })
